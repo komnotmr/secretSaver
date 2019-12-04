bf8e8ffa843a71ec3a99eeb5a7d7b3dfb1d0ac2c
komnotmr <komnotmr@gmail.com>
2019-12-04 20:11:58 +0700

    fix error: __initIP -> _initIP

---
+++ project/modules/brutSecure.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+#~brutSecure.py~
+from datetime import datetime, timedelta
+
+class BrutSecure():
+    def __init__(self, banTimeMinutes=5, intervalTimeMinutes=1, maxRequests=30):
+        self.delayed = datetime.now() - timedelta(weeks=1)
+        self.ipTable = {}
+        self.deltaTime = timedelta(minutes=intervalTimeMinutes)
+        self.limitRequests = maxRequests
+        self.banTime = timedelta(minutes=banTimeMinutes)
+    
+    def _initIP(self):
+        return [0, datetime.now()+self.deltaTime, self.delayed]
+
+    def Add(self, ip):
+        if not ip in self.ipTable.keys():
+            self.ipTable[ip] = self._initIP()
+            return True
+        return False
+    
+    def isBrut(self, ip):
+        if not ip in self.ipTable.keys():
+            return False
+        now = datetime.now()
+        if now < self.ipTable[ip][2]:
+            return True
+        if self.ipTable[ip][0] < self.limitRequests:
+            self.ipTable[ip][0] += 1
+            return False
+        else:
+            if now <= self.ipTable[ip][1]:
+                self.ipTable[ip][0] = 0
+                self.ipTable[ip][1] = now + self.banTime + self.deltaTime
+                self.ipTable[ip][2] = now + self.banTime
+                return True
+            else:
+                self.ipTable[ip] = self._initIP()
+                return False
+    
+    def ShowIps(self):
+        for e in self.ipTable:
+            print(f'{e}:{self.ipTable[e][0]},{self.ipTable[e][1]},{self.ipTable[e][2]}')
+
+
+
+
+
+
+
+
+
+
+
